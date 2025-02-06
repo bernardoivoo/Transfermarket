8faf6c7c6dd391b02862c629b1d3eed753701603
bernardoivoo <99845804+bernardoivoo@users.noreply.github.com>
2025-02-06 20:33:07 -0300

    .

---
+++ src/main/java/Gerenciador/Gerenciarclube.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Gerenciador;
+
+/**
+ *
+ * @author berna
+ */
+public class Gerenciarclube {
+    
+}
