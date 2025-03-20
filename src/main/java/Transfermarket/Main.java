130979efebc7359040125821b24002a322b26c97
CarlosCardosoM <carlos.cardoso@ufvjm.edu.br>
2025-03-20 16:39:03 -0300

    MAin
    Criando a main

--- src/main/java/Transfermarket/Main.java
+++ src/main/java/Transfermarket/Main.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package Transfermarket;
 
-/**
- *
- * @author berna
- */
+public class Main {
+
+    public static void main(String[] args) {
 
 
+    }
+}
